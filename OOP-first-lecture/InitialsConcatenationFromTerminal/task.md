78c8fc5ae00219700b1f0796925495aecfd32418
Dario Di Nucci <d.dinucci@uvt.nl>
2022-09-24 18:07:40 +0200

    Remove exercises with terminal.

--- OOP-first-lecture/InitialsConcatenationFromTerminal/task.md
+++
@@ -1,4 +0,0 @@
-# Concatenare le iniziali prendendo l'input dal terminale
-
-Scrivere un programma che date tre stringhe inserite a linea di comando visualizza la concatenazione delle loro
-iniziali.
--- OOP-first-lecture/PrintMiddleCharFromTerminal/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-   public static void main(String[] args) {
-      char middleChar;
-      middleChar = args[0].charAt(args[0].length()/2);
-      System.out.println(middleChar);
-   }
-}
