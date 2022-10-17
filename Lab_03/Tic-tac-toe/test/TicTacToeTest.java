2b90df9bed2b5adcd9895747ff64d343372c5b06
Dario Di Nucci <d.dinucci@uvt.nl>
2022-10-17 11:01:51 +0200

    Add Andrea's test

--- Lab_03/Tic-tac-toe/test/TicTacToeTest.java
+++ Lab_03/Tic-tac-toe/test/TicTacToeTest.java
@@ -38,4 +38,15 @@ public class TicTacToeTest {
         game.set(0, 2, TicTacToe.player2);
         Assert.assertEquals(TicTacToe.player2, game.getWinner());
     }
+
+    @Test
+    public void AndreaTest() {
+        TicTacToe game = new TicTacToe();
+        game.set(0, 0, TicTacToe.player1);
+        game.set(0, 1, TicTacToe.player2);
+        game.set(1, 0, TicTacToe.player1);
+        game.set(1, 1, TicTacToe.player2);
+        game.set(2, 1, TicTacToe.player1);
+        Assert.assertEquals("Nessun vincitore", game.getWinner());
+    }
 }
